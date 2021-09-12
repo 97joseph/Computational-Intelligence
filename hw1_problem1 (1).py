c2b28dadeaddac2cf5e904ec5f392a2dcbccb095
josephkibira <33089347+97joseph@users.noreply.github.com>
2021-09-12 05:15:41 +0300

    Final

--- hw1_problem1 (1).py
+++ hw1_problem1 (1).py
@@ -6,6 +6,54 @@
 # HW 1, Problem 1
 
 def population(year):
+    #Year to string
+    s=str(year)
+    #getting the last two characters and converting into integer
+    last_two-=10
+    r=last_two*3
+    r+=310
+    return r
+
+    def score(l):
+        total_score=0
+        #2 or 5 in list score will be zero
+        if 2 in l or 5 in l:
+            total_score=0
+        else:
+                for i in l:
+                    total_score=total_score+i
+                    return total_score
+                def factors(n):
+                    l=[]
+                    for i in range(1,n+1):
+                        if n%i==0:
+                            l.append(i)
+                            l.sort()
+                            l.reverse()
+                            return l
+                    def password(i,j,s):
+                        gen_pass=""
+                        gen_pass=str(i)
+
+                    for word in s.split():
+                        gen_pass=gen_pass+word[0]
+                        gen_pass=gen_pass+str(j)
+                        return gen_pass
+                    
+                    def rovarspraket(s):
+                        enc_word=*
+                        for c in s:
+                            if c=='a' or c=='e' or c=='i' or c=='o' or c=='u'
+                            enc_word=enc_word+c
+                            else:
+                                d=c*2
+                                n=d[0]+'o'+d[1]
+                                enc_word=enc_word+n
+                            
+            retun enc_word
+
+
+                    
     # ADD YOUR CODE HERE
     return -1 # CHANGE OR REMOVE THIS LINE
 
